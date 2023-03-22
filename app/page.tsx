011c72a073a0d6a4678fbe7b696cc5088bb3e73c
Oris <orisjb@gmail.com>
2023-03-22 15:09:29 +0000

    search sorted

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 function Home() {
   return (
-    <div>I am the homepage</div>
+    <div className="flex flex-col space-y-10">
+     <Suspense fallback={<p className="text-red-500">Loading the Todos...</p>}>
+    
+    <div className="flex space-x-2">
+      {/* @ts-ignore */}
+      <TodoList/>
+    </div>
+    </Suspense>
+
+<Suspense fallback={<p className="text-blue-500">Loading the Shopping Trollyu...</p>}>
+    <div className="flex space-x-2">
+    {/* @ts-ignore */}
+    <TodoList/>
+    </div>
+    </Suspense>
+    </div>
   )
 }
 
